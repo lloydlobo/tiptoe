e2af52ceb0c12d0170bf8b292224b7c432c35400
Lloyd Lobo <lloydlobo4@gmail.com>
2024-04-14 11:11:07 +0530

    chore: add fps indicator
    - also clear screen with backgroud fill

---
+++ README.md
@@ -0,0 +1,6 @@
+# tiptoe | ldjam 55
+
+## Links
+
+- <https://ldjam.com/events/ludum-dare/55>
+- <https://itch.io/jam/ludum-dare-55-winnipeg>
--- src/tiptoe.py
+++ src/tiptoe.py
@@ -2,10 +2,18 @@ import sys
 
 import pygame as pg
 
+FPS = 60
+
+SCALE = 0.5
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-SCALE = 0.5
-FPS = 60
+SIZE_HALF = (int(SCREEN_WIDTH * SCALE), int(SCREEN_HEIGHT * SCALE))
+
+TILE_SIZE = 16
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+TRANSPARENT = (0, 0, 0, 0)
 
 
 class Game:
@@ -14,14 +22,36 @@ class Game:
 
         pg.display.set_caption("tiptoe")
         self.screen = pg.display.set_mode(SIZE)
+        self.display = pg.Surface(SIZE_HALF, pg.SRCALPHA)
+        self.display_outline = pg.Surface(SIZE_HALF)
+
+        self.font = pg.font.SysFont("monospace", TILE_SIZE)
+
         self.clock = pg.time.Clock()
 
     def run(self) -> None:
+        bg = pg.Surface(SIZE)  # TODO: use actual background image
+        bg.fill(BLACK)
+
         while True:
+            self.display.fill(TRANSPARENT)
+            self.display_outline.blit(bg, (0, 0))
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+
+            # pixel art effect
+            self.screen.blit(
+                pg.transform.scale(self.display_outline, self.screen.get_size()), (0, 0)
+            )
+
+            # show fps
+            text = self.font.render(
+                f"FPS: {self.clock.get_fps():4.0f}", False, WHITE, None
+            )
+            self.screen.blit(text, (TILE_SIZE, TILE_SIZE))
 
             pg.display.flip()
             self.clock.tick(FPS)
