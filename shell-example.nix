5bce94b17a81eb20574b8a197386b3ed89a838de
Lloyd Lobo <lloydlobo4@gmail.com>
2024-07-08 12:18:21 +0530

    build: update shell.nix example

--- shell-example.nix
+++ shell-example.nix
@@ -1,3 +1,71 @@
+# file: shell.nix
+#=========================================================================#
+#
+# To create shell environment and install packages run in project directory
+# where this file is located:
+#
+#   $ nix-shell
+#   $ nix-shell --show-trace
+#
+#=========================================================================#
+let
+  pkgs = import <nixpkgs> {};
+
+  python-with-packages = pkgs.python3.withPackages (ps:
+    with ps; [
+      pygame
+      line_profiler
+      memory_profiler
+      pip
+    ]);
+
+  altgraph = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "altgraph";
+    version = "0.17.3"; # You can update to the latest version
+    src = pkgs.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "rTM1gRTffJQWzbj6HqpYUhZsUFEYcXAhxqjHx6u9A90=";
+    };
+    doCheck = false;
+  };
+
+  pyinstaller = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "pyinstaller";
+    version = "5.13.2"; # You can update to the latest version
+    src = pkgs.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "yOXTSJw6fMX4QBwtH0inDliPmWfjkcOwbdrB9oX41dI=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      setuptools
+
+      # NOTE: If `altgraph` not available in your current nixpkgs, we need to
+      # define it separately, similar to pyinstaller above.
+      altgraph
+    ];
+    buildInputs = [
+      pkgs.zlib
+    ];
+    preBuild = ''
+      export PYI_STATIC_ZLIB=1
+    '';
+  };
+in
+  pkgs.mkShell {
+    packages = [
+      python-with-packages
+      pyinstaller
+    ];
+    shellHook = ''
+      echo "Python environment ready with PyInstaller"
+      python --version
+      pyinstaller --version
+    '';
+  }
+/*
+20240708064749UTC
+
 # shell.nix
 #=========================================================================#
 #
@@ -29,3 +97,5 @@ in
       ]))
     ];
   }
+*/
+
